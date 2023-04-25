46426a3941f2774ef156b7c5e305b7fa3bbd0646
Arim Jossé <arimjosse@googlemail.com>
2023-04-25 12:42:41 +0200

    added ingredient list item boilerplate

---
+++ frontend/src/components/IngredientListItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="">
+      <h3>{{ name }}</h3>
+      <ul>
+        <li>Fats: {{ fats }}</li>
+        <li>Calories: {{ calories }}</li>
+        <li>Proteins: {{ proteins }}</li>
+        <li>Carbohydrates: {{ carbohydrates }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      props: {
+        name: {
+          type: String,
+          required: true
+        },
+        fats: {
+          type: Number,
+          default: 0
+        },
+        calories: {
+          type: Number,
+          default: 0
+        },
+        proteins: {
+          type: Number,
+          default: 0
+        },
+        carbohydrates: {
+          type: Number,
+          default: 0
+        }
+      }
+    };
+  </script>
+  
+  <style>
+
+  </style>
+  
