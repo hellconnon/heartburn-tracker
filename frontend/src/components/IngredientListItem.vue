d2b65b8d9ed586ac990ca48c47bfa8b9ce2f102d
Arim Jossé <arim.josse@adweko.com>
2023-05-02 01:05:56 +0200

    current work state

--- frontend/src/components/IngredientListItem.vue
+++ frontend/src/components/IngredientListItem.vue
@@ -1,43 +1,48 @@
 <template>
-    <div class="">
-      <h3>{{ name }}</h3>
-      <ul>
-        <li>Fats: {{ fats }}</li>
-        <li>Calories: {{ calories }}</li>
-        <li>Proteins: {{ proteins }}</li>
-        <li>Carbohydrates: {{ carbohydrates }}</li>
-      </ul>
+  <div class="">
+    <div class="w-8 h-8 border border-gray-300 rounded-full flex items-center justify-center"
+      @click="isChecked = !isChecked">
+      <div v-if="isChecked" class="bg-green-500 w-6 h-6 rounded-full flex items-center justify-center">
+        <i class="fas fa-check text-white"></i>
+      </div>
     </div>
-  </template>
-  
-  <script>
-    export default {
-      props: {
-        name: {
-          type: String,
-          required: true
-        },
-        fats: {
-          type: Number,
-          default: 0
-        },
-        calories: {
-          type: Number,
-          default: 0
-        },
-        proteins: {
-          type: Number,
-          default: 0
-        },
-        carbohydrates: {
-          type: Number,
-          default: 0
-        }
-      }
-    };
-  </script>
-  
-  <style>
 
-  </style>
+    <h3>{{ ingredientName }}</h3>
+    <ul>
+      <li>Fats: {{ fats }}</li>
+      <li>Calories: {{ calories }}</li>
+      <li>Proteins: {{ proteins }}</li>
+      <li>Carbohydrates: {{ carbohydrates }}</li>
+    </ul>
+  </div>
+</template>
+  
+<script>
+export default {
+  props: {
+    ingredientName: {
+      type: String,
+      required: true
+    },
+    fats: {
+      type: Number,
+      default: 0
+    },
+    calories: {
+      type: Number,
+      default: 0
+    },
+    proteins: {
+      type: Number,
+      default: 0
+    },
+    carbohydrates: {
+      type: Number,
+      default: 0
+    }
+  }
+};
+</script>
+  
+<style></style>
   
--- frontend/src/components/Ingredients.vue
+++ frontend/src/components/Ingredients.vue
@@ -1,21 +1,60 @@
 <template>
-<div class="flex">
-    <Sidebar/>
+    <div class="flex">
+        <Sidebar />
+        <div class="pl-8 pt-4 grid grid-clos-3 gap-4 h-full w-full">
+            <div class="cols-span-1">
+                <div class="flex items-center justify-between mb-4">
+
+                    <h2 class="sticky text-3xl text-white font-bold">Ingredients</h2>
+                    <div class="flex items-center justify-end">
+                        <button
+                            class="fas fa-plus text-white border rounded-full w-8 h-8 hover:bg-white hover:text-blue-700 mr-2"
+                            @click="showAddIngredient = true"></button>
+                        <button
+                            class="fas fa-filter text-white border rounded-full w-8 h-8 hover:bg-white hover:text-blue-700 mr-2"></button>
+                        <button
+                            class="fas fa-sort text-white border rounded-full w-8 h-8 hover:bg-white hover:text-blue-700"></button>
+
+                    </div>
+                </div>
+                <div class="space-y-2">
+          <IngredientListItem
+            v-for="(ingredient, index) in ingredients"
+            :key="index"
+            :ingredient-name="ingredient.name"
+            :ingredient-count="ingredient.count"
+            :ingredient-type="ingredient.type"
+            :selected="index === selectedIndex"
+            @select="selectingredient(index, ingredient)"
+          />
+        </div>
+            </div>
+            <div class="col-span-2 p-4 rounded-3xl text-white">
+
+            </div>
+        </div>
 
 
-</div>
-
+    </div>
 </template>
 
 <script>
 import Sidebar from "./Sidebar.vue"
+import IngredientListItem from "./IngredientListItem.vue";
 
 export default {
     name: "Ingredients",
-    components: {Sidebar}
+    components: { Sidebar, IngredientListItem },
+    data() {
+        return {
+            showAddIngredient: false,
+            selectedIndex: null,
+            ingredients: [
+                {name: "Test", fats: 1, calories: 2, proteins: 3, carbohydrates: 4},
+            ]
+        }
+    }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
